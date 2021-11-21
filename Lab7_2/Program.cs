4a05b13c40fe3023ff35683ed2a975e0493229a3
Татьяна Анисимова <kulmina@mail.ru>
2021-11-21 10:56:32 +0300

    Добавьте файлы проекта.

---
+++ Lab7_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double S, V;
+            GetParam(a, out S, out V);
+            Console.WriteLine("Площадь куба");
+            Console.WriteLine(S);
+            Console.WriteLine("Объем куба");
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+        static void GetParam(int a, out double S, out double V)
+        {
+            S = 6 * a * a;
+            V = ((double)a * a * a);
+        }
+
+    }
+}
